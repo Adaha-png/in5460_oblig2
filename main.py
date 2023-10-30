2c82301de95b938966a119b1bc100abb4aabed40
Adaha-png <ada.hatland@gmail.com>
2023-10-30 23:02:27 +0100

    model as given by problem description

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import pytablereader as ptr
+import pytablewriter as ptw
+from model import Model
+
+def main():
+    file_path = "sample_data.csv"
+    with open(file_path, "w") as f:
+        f.write(csv_text)
+
+    loader = ptr.CsvTableFileLoader(file_path)
+    for table_data in loader.load():
+        print ("\n".join([
+        "load from file",
+        "==============",
+        "{:s}".format(ptw.dumps_tabledata(table_data)),
+        ]))
+
 if __name__=="__main__":
     main()
