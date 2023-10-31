c1aff07b97ec34cc8bd95cc867bc35b490ac7cd0
Adaha-png <ada.hatland@gmail.com>
2023-10-31 19:23:43 +0100

    more stuff

--- main.py
+++ main.py
@@ -1,54 +1,33 @@
-import pytablereader as ptr
-import pytablewriter as ptw
-from model import Model, LSTM
+from model import SimpleRNN, LSTM
 import tensorflow as tf
 import tensorflow_federated as tff
 import argparse
+import pandas as pd
+import numpy as np
 
 def main():
-    file_path = "sample_data.csv"
-    with open(file_path, "w") as f:
-        f.write(csv_text)
+    file_path = "Dataset for assignment 2.xlsx"
+    df = pd.read_excel(file_path)
+    total = df.sum(axis='columns').to_numpy()
 
-    loader = ptr.CsvTableFileLoader(file_path)
-    for table_data in loader.load():
-        print ("\n".join([
-        "load from file",
-        "==============",
-        "{:s}".format(ptw.dumps_tabledata(table_data)),
-        ]))
+    print(total)
+    rnn_model = SimpleRNN()
+    rnn_model.build_default((7*96,1), 96, activation = None)
 
-# Load simulation data .
-source, _ = tff.simulation.datasets.emnist.load_data()
-def client_data(n):
-    return source.create_tf_dataset_for_client(source.client_ids[n]).map( lambda e:(tf.reshape(e[’pixels’], [-1]), e[’label’])).repeat(10).batch(20)
-# Pick a subset of client devices to participate in training .
-train_data = [client_data(n) for n in range(3)]
-# Wrap a Keras model for use with TFF.
+    lstm_model = LSTM()
+    lstm_model.build_default((7*96,1), 96, activation = None)
 
-def model_fn (args, training = True):
-    model = Model(args, training)
+    trainer = tff.learning.algorithms.build_unweighted_fed_avg(model_fn(rnn_model), client_optimizer_fn = lambda: tf.keras.optimizers.SGD(0.1))
+    state = trainer.initialize()
+
+    for _ in range(300):
+        state, metrics = trainer.next(state,train_data)
+        print(metrics['train']['loss'])
+    
+def model_fn (model, training = True):
     return tff.learning.from_keras_model(model, input_spec = train_data[0].element_spec, loss = tf.keras.losses.SparseCategoricalCrossentropy(), metrics =[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-args = argparse.ArgumentParser(
-        model = 'rnn',
-        num_layers = 3,
-        rnn_size = 4,
-        output_keep_prob = 1,
-        input_keep_prob = 1,
-        batch_size = 64,
-        seq_length = 129438,
-        vocab_size = 214,
-        grad_clip = 1,
-        )
-
 # Simulate a few rounds of training with the selected client devices .
-trainer = tff.learning.build_federated_averaging_process(model_fn(args), client_optimizer_fn = lambda: tf.keras.optimizers.SGD(0.1))
-state = trainer.initialize()
-
-for _ in range(5):
-    state, metrics = trainer.next(state,train_data)
-    print(metrics[’train’][’loss’])
 
 if __name__=="__main__":
     main()
