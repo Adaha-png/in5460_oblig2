afdfee053b34e6bfc41593fb024a25efc0d67bb5
Benjami <ben.overgaard@gmail.com>
2023-11-09 19:42:04 +0100

    update plot.py
    la til ground truth vs prediction plots

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plotter (timestep, loss,classification, rnn):
+def plotter (timestep, loss,prediction, actual, classification: bool, rnn:bool):
     if(classification):
         if(rnn):
             plt.plot(timestep, loss, "r", label="CrossEntropy loss")
@@ -11,12 +11,30 @@ def plotter (timestep, loss,classification, rnn):
             plt.ylabel("Loss", fontdict={"size":12, "family":"serif"})
             plt.legend()
             plt.show()
+
+            plt.plot(timestep, actual, 'g', label = "Ground truth")
+            plt.plot(timestep, prediction, 'b', label = "Predicted output")
+            plt.grid()
+            plt.title("Predicted vs Ground Truth using RNN")
+            plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
+            plt.ylabel("kWh", fontdict={"size":12, "family":"serif"})
+            plt.legend()
+            plt.show()
         else:
             plt.plot(timestep, loss, "r", label="CrossEntropy loss")
             plt.grid()
             plt.title("Classification model using LSTM")
             plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
             plt.ylabel("Loss", fontdict={"size":12, "family":"serif"})
+            plt.legend()
+            plt.show()
+
+            plt.plot(timestep, actual, 'g', label = "Ground truth")
+            plt.plot(timestep, prediction, 'b', label = "Predicted output")
+            plt.grid()
+            plt.title("Predicted vs Ground Truth using LSTM")
+            plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
+            plt.ylabel("kWh", fontdict={"size":12, "family":"serif"})
             plt.legend()
             plt.show()
     else:
@@ -28,11 +46,29 @@ def plotter (timestep, loss,classification, rnn):
             plt.ylabel("Loss", fontdict={"size":12, "family":"serif"})
             plt.legend()
             plt.show()
+
+            plt.plot(timestep, actual, 'g', label = "Ground truth")
+            plt.plot(timestep, prediction, 'b', label = "Predicted output")
+            plt.grid()
+            plt.title("Predicted vs Ground Truth using RNN")
+            plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
+            plt.ylabel("kWh", fontdict={"size":12, "family":"serif"})
+            plt.legend()
+            plt.show()
         else:
             plt.plot(timestep, loss, "r", label="Mean Squared Error")
             plt.grid()
             plt.title("Prediction model using LSTM")
             plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
             plt.ylabel("Loss", fontdict={"size":12, "family":"serif"})
+            plt.legend()
+            plt.show()
+
+            plt.plot(timestep, actual, 'g', label = "Ground truth")
+            plt.plot(timestep, prediction, 'b', label = "Predicted output")
+            plt.grid()
+            plt.title("Predicted vs Ground Truth using LSTM")
+            plt.xlabel("Timestep", fontdict={"size":12, "family":"serif"})
+            plt.ylabel("kWh", fontdict={"size":12, "family":"serif"})
             plt.legend()
             plt.show()
